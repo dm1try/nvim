c03d4df0de374364e465155a23fc918ae38c4c7b
dm1try <me@dmitry.it>
2016-10-16 03:13:11 +0300

    add ElixirReloadScript debug command

--- lib/host/plugin.ex
+++ lib/host/plugin.ex
@@ -16,4 +16,20 @@ defmodule NVim.Host.Plugin do
     log_path = Application.get_env(:logger, :error_log)[:path]
     nvim_command "bot new | res 15 | set wfh | term tail -f #{log_path}"
   end
+
+  command elixir_reload_script do
+    with {:ok, buffer} <- nvim_get_current_buf(),
+         {:ok, filename} <- nvim_buf_get_name(buffer) do
+      try do
+        Code.load_file(filename)
+        nvim_command "echo 'Reloaded.'"
+      rescue
+        any ->
+          nvim_command "echo 'Problem with reloading: #{inspect any}'"
+      end
+    else
+      error ->
+        nvim_command "echo 'Problem with reloading: #{inspect error}'"
+    end
+  end
 end
