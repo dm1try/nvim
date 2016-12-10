2c829d0eaab4ce1ec24d7fbe23ca9352c55fb6a7
dm1try <me@dmitry.it>
2016-12-10 21:57:26 +0300

    set needed data paths using env vars in integration test helper

--- lib/nvim/test/integration.ex
+++ lib/nvim/test/integration.ex
@@ -8,7 +8,7 @@ defmodule NVim.Test.Integration do
     nvim_config_path = "#{fixtures_path}/xdg_home/nvim"
 
     create_file "#{nvim_config_path}/init.vim", init_rc_content, force: true
-    System.cmd "mix", ["nvim.install", nvim_config_path]
+    System.cmd "mix", ["nvim.install", nvim_config_path], env: install_env(fixtures_path)
   end
 
   def start_host_session(fixtures_path, session_name \\ IntegrationTest.Session) do
@@ -22,5 +22,10 @@ defmodule NVim.Test.Integration do
   def remove_host(fixtures_path) do
     nvim_config_path = "#{fixtures_path}/xdg_home/nvim"
     System.cmd "mix", ["nvim.remove", nvim_config_path]
+  end
+
+  defp install_env(fixtures_path) do
+    [{"XDG_DATA_PATH", "#{fixtures_path}/xdg_data"},
+     {"NVIM_RPLUGIN_MANIFEST", "#{fixtures_path}/xdg_data/rplugin.vim"}]
   end
 end
