08fd194900545caf81c69cde406a472edfeed124
dm1try <me@dmitry.it>
2017-07-02 21:48:16 +0300

    format syntax

--- installer/lib/install.ex
+++ installer/lib/install.ex
@@ -11,17 +11,17 @@ defmodule Mix.Tasks.Nvim.Install do
       [] ->
         Mix.raise "Expected NVIM_CONFIG_PATH to be given, please use \"mix nvim.install NVIM_CONFIG_PATH\""
       [nvim_config_path | _] ->
-        create_file Path.join(nvim_config_path, "plugin/elixir_host.vim"), elixir_host_plugin_vim_text, force: true
+        create_file Path.join(nvim_config_path, "plugin/elixir_host.vim"), elixir_host_plugin_vim_text(), force: true
 
         remote_plugin_path = Path.join(nvim_config_path, "rplugin/elixir")
 
         create_directory Path.join(remote_plugin_path, "scripts")
         create_directory Path.join(remote_plugin_path, "apps")
-        create_file Path.join(remote_plugin_path, "mix.exs"), apps_mixfile_text, force: true
-        create_file Path.join([remote_plugin_path, "config", "config.exs"]), apps_config_text, force: true
+        create_file Path.join(remote_plugin_path, "mix.exs"), apps_mixfile_text(), force: true
+        create_file Path.join([remote_plugin_path, "config", "config.exs"]), apps_config_text(), force: true
 
         case System.cmd("mix", ["do", "deps.get,", "nvim.build_host"], cd: remote_plugin_path) do
-          {_result, 0} -> print_successful_info
+          {_result, 0} -> print_successful_info()
           {result, _} -> print_error("#{inspect result}")
           _ -> print_error("Something was going wrong.")
         end
